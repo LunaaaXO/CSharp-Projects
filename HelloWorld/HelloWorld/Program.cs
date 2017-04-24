f4d709f44dd1de93d0c655c16850d4ed2a2f871e
Seraphine <applejack111luvsu@gmail.com>
2017-04-24 12:38:41 +0200

    Hello World ConsoleApp

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApplication
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            /* My First C# Console App */
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
