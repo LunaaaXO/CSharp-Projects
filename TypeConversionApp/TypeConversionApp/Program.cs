5ec80b6fbab126dba77b39c93f998933aa804dbc
Amelie <applejack111luvsu@gmail.com>
2017-05-11 14:34:21 +0200

    TypeConversionApp
    TypeConversion Application

---
+++ TypeConversionApp/TypeConversionApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversionApp
+{
+    class ExplicitConversion
+    {
+        static void Main(string[] args)
+        {
+            double d = 5673.74;
+            int i;
+
+            // Cast double to int
+            i = (int)d;
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
