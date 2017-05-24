502f353d37c00acc0134b92c6135445a3dbe8207
Seraphine <seraphinethepanda@gmail.com>
2017-05-24 17:53:55 +0200

    DeclaringConstantsApp
    DeclaringConstants Application

---
+++ DeclaringConstantsApp/DeclaringConstantsApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeclaringConstantsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14159; // Constant declaration
+            double r;
+            Console.WriteLine("Enter Radius: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            double areaCircle = pi * r * r;
+            Console.WriteLine("Radius: {0}, Area: {1}", r, areaCircle);
+            Console.ReadLine();
+        }
+    }
+}
